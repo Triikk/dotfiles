f0596bdd23d0d7f6eece7b4810df6c082cc79dd5
Mattia Ronchi <mattia.ronchi5@studio.unibo.it>
2024-02-23 20:59:02 +0100

    added minimal neovim configuration

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,6 @@
+local plugins = {
+	"ellisonleao/gruvbox.nvim",
+	"rebelot/kanagawa.nvim"
+}
+
+require("lazy").setup(plugins)
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import sys
 HOME_DIR = os.environ.get("HOME")
 PATHS : dict[str, list[str]] = {
     "" : [".vimrc", ".bashrc", ".bash_profile", ".Xdefaults"],
-    ".config": ["gammastep", "neofetch", "sway", "waybar", "bat", "gdb", "user-dirs.dirs", "user-dirs.locale"],
+    ".config": ["gammastep", "neofetch", "sway", "waybar", "bat", "gdb", "nvim", "user-dirs.dirs", "user-dirs.locale"],
     ".vim": ["coc-settings.json", "config"],
 }
 DEBUG : bool
