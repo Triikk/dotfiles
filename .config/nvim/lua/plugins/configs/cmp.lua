91cb0b0de63096e7022fa659814ff6e27cbe2342
Mattia Ronchi <mattia.ronchi5@studio.unibo.it>
2024-02-24 12:46:44 +0100

    added lsp configuration + minimal code completion

---
+++ .config/nvim/lua/plugins/configs/cmp.lua
@@ -0,0 +1,36 @@
+local cmp = require("cmp")
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+    }, {
+        { name = 'buffer' },
+    }),
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require("lspconfig").pyright.setup {
+    capabilities = capabilities
+}
+require("lspconfig").rust_analyzer.setup {
+    capabilities = capabilities
+}
+require("lspconfig").clangd.setup {
+    capabilities = capabilities
+}
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -6,9 +6,27 @@ local plugins = {
     "rebelot/kanagawa.nvim",
 
     {
+        "neovim/nvim-lspconfig",
+    },
+    {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate"
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl"
+    },
+    {            
+        "L3MON4D3/LuaSnip",
+        dependencies = "rafamadriz/friendly-snippets"
+    },
+    {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/nvim-cmp",
     }
 }
 
 require("lazy").setup(plugins)
+require("ibl").setup()
+require "plugins.configs.lspconfig"
+require "plugins.configs.cmp"
