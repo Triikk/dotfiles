0d6752d5e8eb30d67843d2e7d45ec8610c7a90c5
Mattia Ronchi <mattia.ronchi5@studio.unibo.it>
2024-02-24 18:11:34 +0100

    further updates (options + plugins)

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,3 +1,18 @@
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
+
+vim.wo.number = true
+
+vim.o.mouse = "a"
+
+-- clipboard synced with OS
+vim.o.clipboard = "unnamedplus"
+
+vim.o.ignorecase = true
+-- case sensitive if contains an uppercase letter
+vim.o.smartcase = true
+
+vim.wo.signcolumn = "yes"
+
+vim.o.termguicolors = true
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,13 +1,16 @@
 local plugins = {
+    -- git
+    "tpope/vim-fugitive",
+    "tpope/vim-rhubarb",
+
     -- color schemes
     "ellisonleao/gruvbox.nvim",
     "sainnhe/gruvbox-material",
     "sainnhe/everforest",
     "rebelot/kanagawa.nvim",
 
-    {
-        "neovim/nvim-lspconfig",
-    },
+    "neovim/nvim-lspconfig",
+    "folke/neodev.nvim",
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate"
@@ -18,11 +21,30 @@ local plugins = {
     },
     {            
         "L3MON4D3/LuaSnip",
-        dependencies = "rafamadriz/friendly-snippets"
+        build = "make install_jsregexp"
+    },
+    {
+        "saadparwaiz1/cmp_luasnip",
+        "rafamadriz/friendly-snippets"
     },
     {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-path"
+    },
+    {
+        "folke/which-key.nvim"
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        opts = {
+            options = {
+                icons_enabled = true,
+                theme = "gruvbox-material",
+                component_separators = "|",
+                section_separators = "",
+            },
+        }
     }
 }
 
